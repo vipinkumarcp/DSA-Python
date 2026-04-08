9941efa4a08571705cf49bb5f2c66ee83b616fd5
vipin <vipinkumarcp4@gmail.com>
2026-04-08 08:05:14 +0530

    fib last number

---
+++ Algorithms/fiboacci_n.py
@@ -0,0 +1,27 @@
+
+
+def fib(n,k=1):
+
+    if n <=1:
+        return n
+
+    if k == 1:
+        period = 60
+    else :
+        period = 15 * (10 ** (k-1))
+
+    n = n % period
+
+    mod = 10 ** k
+
+
+    a,b = 0,1
+
+    for _ in range(n):
+
+        a,b = b , (a+b) % mod
+
+    return a     
+
+
+print(fib(20))
