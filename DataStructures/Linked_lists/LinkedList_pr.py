686e7c2d6915ce924b514febd0c4ec2701c5e6bf
vipin <vipinkumarcp4@gmail.com>
2026-07-04 16:54:45 +0530

    practice

--- DataStructures/Linked_lists/LinkedList_pr.py
+++ DataStructures/Linked_lists/LinkedList_pr.py
@@ -1,36 +1,33 @@
+
 class Node:
 
     def __init__(self,data):
-
         self.data = data
-        self.next_node = None
-
-    def __repr__(self):
-        return str(self.data)
+        self.next_node =None
 
 
 class LinkedList:
 
     def __init__(self):
-
-        self.no_of_nodes = 0
         self.head = None
+        self.no_of_nodes = None
 
+    
     def insert_start(self,data):
 
-        self.no_of_nodes += 1
         new_node = Node(data)
 
-        if self.head is None:
-            self.head = new_node
+        if not self.head:
 
+            self.head = new_node
         else:
+
             new_node.next_node = self.head
             self.head = new_node
 
+
     def insert_end(self,data):
 
-        self.no_of_nodes += 1
         new_node = Node(data)
 
         actual_node = self.head
@@ -45,76 +42,46 @@ class LinkedList:
     def remove(self,data):
 
         actual_node = self.head
+
         previous_node = None
 
-        while actual_node is not None  and actual_node.data != data:
+        while actual_node.next_node is not None and actual_node.data != data:
 
             previous_node = actual_node
+
             actual_node = actual_node.next_node
 
-        if actual_node is None:
-
-            return
-
-        self.no_of_nodes -= 1
-
-        if previous_node == None:
+        if previous_node is None:
 
             self.head = actual_node.next_node
 
         else:
-           
-            
+
             previous_node.next_node = actual_node.next_node
 
-    
-    def sizeOfList(self):
-        return self.no_of_nodes
-    
 
-    def traverse(self):
+    def middle_node(self,data):
 
-        actual_node = self.head
+        fast_pointer = self.head
 
-        while actual_node is not None:
+        slow_pointer = self.head
 
-            print(actual_node)
-            actual_node = actual_node.next_node
+        while fast_pointer and fast_pointer.next_node:
 
-       
+            slow_pointer = slow_pointer.next_node
 
+            fast_pointer = fast_pointer.next_node.next_node
 
-
-linked_list = LinkedList()
-linked_list.insert_start(4)
-linked_list.insert_start(3)
-linked_list.insert_start(7)
-linked_list.insert_end(18)
-linked_list.insert_end(10)
-
-
-linked_list.insert_start('Adam')
-linked_list.traverse()
-print('size:', linked_list.sizeOfList())
-print('//////////////')
-linked_list.remove('Adam')
-linked_list.traverse()
-print('size', linked_list.sizeOfList())
-print('//////////////')
-linked_list.remove(10)
-linked_list.traverse()
-linked_list.traverse()
+        return slow_pointer
 
 
 
 
 
 
+        
 
 
 
+        
 
-
-
-
-    
